2e40f585ae8f7963a7a63323909bcb34d28aca3a
Alexis Luengas <alexis.luengas@gmail.com>
2014-12-02 16:51:59 +0100

    Minor changes to the interface
    Output now defaults to the directory of the input file

--- desktop/cavatina/translator.py
+++ desktop/cavatina/translator.py
@@ -10,7 +10,7 @@ def create_m21Note(nobj):
     if nobj.is_tied():
         n.tie = tie.Tie("start")
     return n
-    
+
 def extractMergedDiacritics(n1, n2):
     artic = n1.articulations
     for a in n2.articulations:
@@ -18,23 +18,23 @@ def extractMergedDiacritics(n1, n2):
             n1.articulations.append(a)
     n1.articulations = []
     n2.articulations = []
-    
+
     expre = n1.expressions
     for a in n2.expressions:
         if a not in n1.expressions:
             n1.expressions.append(a)
     n1.expressions = []
     n2.expressions = []
-    
+
     return [artic, expre]
 
 def translateToMusic21(tree, preserveStemDirection=False):
     score = stream.Score()
-    
+
     score.insert(metadata.Metadata())
     # score.metadata.title = 'Untitled'
     # score.metadata.composer = 'Unknown Composer'
-    
+
     score.append(stream.Part())
     part = score[-1]
     part.append(stream.Measure()) # measure['current'] is always already contained in part
@@ -45,34 +45,34 @@ def translateToMusic21(tree, preserveStemDirection=False):
     lastKeySign =   {0 : None, 1 : None, 2 : None, 3 : None, 4 : None} # part number : int
     lastClef =      {0 : None, 1 : None, 2 : None, 3 : None, 4 : None}
     lastTimeSign =  {0 : None, 1 : None, 2 : None, 3 : None, 4 : None}
-    
+
     # states
     octavationSwitch = False
     catchPartitioning = False # watch-state triggered by a Newline
-    
+
     def insertNewMeasure():
         part.append( stream.Measure() )
         measure['current'] = part[-1]
         measure['counter'] += 1
         measure['current'].number = measure['counter']
-    
+
     for structure in tree:
         # (time structures)
         if isinstance(structure, Chord):
             if len(structure) > 1:
                 noteList = []
-                
+
                 for nobj in structure:
                     n = create_m21Note(nobj)
                     if preserveStemDirection:
                         n.stemDirection = nobj.stem_direction
                     noteList.append(n)
-                
+
                 # Trim chord into chord voices
                 chordVoices = [ [noteList[0]] ]
                 currentLength = noteList[0].quarterLength
                 currentVoice = chordVoices[0]
-                
+
                 for n in noteList[1:]:
                     if n.quarterLength != currentLength:
                         chordVoices.append([n])
@@ -80,7 +80,7 @@ def translateToMusic21(tree, preserveStemDirection=False):
                         currentVoice = chordVoices[-1]
                     else:
                         currentVoice.append(n)
-                
+
                 # Make chords
                 for nlist in chordVoices:
                     c = chord.Chord(nlist)
@@ -92,13 +92,13 @@ def translateToMusic21(tree, preserveStemDirection=False):
                         chordOffset = measure['current'][-1].offset
                     else:
                         measure['current'].insert(chordOffset, c ) # overlap
-                
+
                 if len(chordVoices) > 1: # if overlap
                     makeVoices.append( (part.id, measure['current'].number) ) # remember overlap locations
-                    
+
             else:
                 measure['current'].append( create_m21Note(structure[0]) )
-            
+
             # Beams
             if structure.beamToPrevious:
                 try:
@@ -106,19 +106,19 @@ def translateToMusic21(tree, preserveStemDirection=False):
                     measure['current'][-1].beams.fill('eighth', type='stop')
                 except stream.StreamException:
                     pass # TODO: handle StreamException
-                
+
             # Ottava
             if octavationSwitch:
                 measure['current'][-1].transpose('p8', inPlace=True)
                 ottava.addSpannedElements(measure['current'][-1])
-            
+
             continue
-                
+
         if isinstance(structure, Rest):
             measure['current'].append( note.Rest(quarterLength=structure.get_quarterLength()) )
             continue
         # (end time structures)
-        
+
         # (signatures)
         if isinstance(structure, KeySignature):
             if catchPartitioning:
@@ -127,18 +127,18 @@ def translateToMusic21(tree, preserveStemDirection=False):
                 measure['counter'] = part[-1].number
                 insertNewMeasure()
                 catchPartitioning = False
-            
+
             i = score.index(part)
             newClef = structure.get_m21clef()()
             newKeySign = structure.getm21signature()
-            
+
             if newKeySign != lastKeySign[i] or newClef.sign != lastClef[i]:
                 lastKeySign[i] = newKeySign
                 lastClef[i] = newClef.sign
                 measure['current'].clef = newClef # instantiation
                 measure['current'].insert(0.0, key.KeySignature(newKeySign) )
             continue
-        
+
         if isinstance(structure, TimeSignature):
             newTimeSignature = meter.TimeSignature(structure.get_m21fractionalTime())
             i = score.index(part)
@@ -147,7 +147,7 @@ def translateToMusic21(tree, preserveStemDirection=False):
                 measure['current'].insert(0.0, newTimeSignature)
             continue
         # (end signatures)
-        
+
         # (barlines)
         if isinstance(structure, MeasureEnd):
             if catchPartitioning and not isinstance(structure, SystemicBarline):
@@ -156,26 +156,26 @@ def translateToMusic21(tree, preserveStemDirection=False):
                 measure['counter'] = part[-1].number
                 insertNewMeasure()
                 catchPartitioning = False
-                
+
             elif len(measure['current']) > 0:
                 insertNewMeasure()
-        
+
         if isinstance(structure, SectionEnd):
             measure['current'].append( bar.Barline(style='double') )
             insertNewMeasure()
             continue
-        
+
         if isinstance(structure, End):
             if len(repeatEnds) != 0: # close last repeat section
                 startMeasureNo, endingNo = repeatEnds.pop()
                 endMeasureNo = measure['current'].number
                 repeat.insertRepeatEnding(part, startMeasureNo, endMeasureNo, endingNumber=endingNo, inPlace=True)
-                
+
             measure['current'].append( bar.Barline(style='final') )
             insertNewMeasure()
             continue
         # (end barlines)
-        
+
         # (dynamics)
         if isinstance(structure, Dynamic):
             if len(measure['current']) > 0 and isinstance(measure['current'][-1], chord.Chord) or isinstance(measure['current'][-1], note.Note):
@@ -184,7 +184,7 @@ def translateToMusic21(tree, preserveStemDirection=False):
             else:
                 measure['current'].append( dynamics.Dynamic(structure.get_m21dynamic()) )
             continue
-        
+
         if isinstance(structure, GradualDynamic):
             if len(measure['current']) > 0 and isinstance(measure['current'][-1], chord.Chord):
                 lastChordOffset = measure['current'][-1].offset
@@ -199,24 +199,24 @@ def translateToMusic21(tree, preserveStemDirection=False):
                     measure['current'].append( dynamics.Diminuendo() )
             continue
         # (end dynamics)
-        
+
         # (repeat structures)
         if isinstance(structure, RepeatFrom):
             if len(measure['current']) > 0 and not isinstance(measure['current'][-1], bar.Repeat):
                 insertNewMeasure()
             measure['current'].leftBarline = bar.Repeat(direction='start')
             continue
-            
+
         if isinstance(structure, RepeatTo):
             if len(repeatEnds) != 0: # close last repeat section
                 startMeasureNo, endingNo = repeatEnds.pop()
                 endMeasureNo = measure['current'].number
                 repeat.insertRepeatEnding(part, startMeasureNo, endMeasureNo, endingNumber=endingNo, inPlace=True)
-            
+
             measure['current'].rightBarline = bar.Repeat(direction='end')
             insertNewMeasure()
             continue
-        
+
         if isinstance(structure, RepeatSectionStart):
             if len(repeatEnds) != 0: # close last repeat section
                 startMeasureNo, endingNo = repeatEnds.pop()
@@ -225,14 +225,14 @@ def translateToMusic21(tree, preserveStemDirection=False):
                 else:
                     endMeasureNo = measure['current'].number
                 repeat.insertRepeatEnding(part, startMeasureNo, endMeasureNo, endingNumber=endingNo, inPlace=True)
-            
+
             endingNo = structure.get_m21no()
             if len(measure['current']) == 0:
                 repeatEnds.append([measure['current'].number, endingNo])
             else:
                 repeatEnds.append([measure['current'].number + 1, endingNo])
             continue
-            
+
         if isinstance(structure, RepeatSectionEnd):
             startMeasureNo, endingNo = repeatEnds.pop()
             if len(measure['current']) == 0:
@@ -241,18 +241,18 @@ def translateToMusic21(tree, preserveStemDirection=False):
                 endMeasureNo = measure['current'].number
             repeat.insertRepeatEnding(part, startMeasureNo, endMeasureNo, endingNumber=endingNo, inPlace=True)
             continue
-            
+
         if isinstance(structure, Coda):
             measure['current'].append( repeat.Coda() )
             continue
-        
+
         if isinstance(structure, Segno):
             measure['current'].append( repeat.Segno() )
             continue
-        
+
         if isinstance(structure, FromTo):
             repfrom, repto = structure.get_m21parameters()
-            
+
             if repfrom == 'D.C.':
                 if repto == None:
                     measure['current'].append( repeat.DaCapo() )
@@ -269,7 +269,7 @@ def translateToMusic21(tree, preserveStemDirection=False):
                     measure['current'].append( repeat.DalSegnoAlFine() )
             continue
         # (end repeat structures)
-        
+
         #(ottava)
         if isinstance(structure, OctavationStart):
             otp = structure.octaveTranspositions
@@ -284,13 +284,13 @@ def translateToMusic21(tree, preserveStemDirection=False):
                 ottava = spanner.Ottava(type='15ma')
                 octavationSwitch = True
             continue
-            
+
         if isinstance(structure, OctavationEnd):
             part.insert(0.0, ottava)
             octavationSwitch = False
             continue
         #(end ottava)
-        
+
         #(sustain pedal)
         #
         # A corresponding music21.spanner object is not yet implemented in the music21 library.
@@ -300,7 +300,7 @@ def translateToMusic21(tree, preserveStemDirection=False):
         # if isinstance(structure, PedalUp):
         #
         #(end sustain pedal)
-        
+
         #(grand staff)
         if isinstance(structure, GrandStaff) or isinstance(structure, SystemicBarline):
             if catchPartitioning:
@@ -311,13 +311,13 @@ def translateToMusic21(tree, preserveStemDirection=False):
                     part.offset = 0.0
                     measure['counter'] = -1
                     insertNewMeasure()
-                    
+
                     if isinstance(structure, GrandStaff):
                         p1 = score.parts[-2]
                         p2 = score.parts[-1] # = part
                         staffGroup = layout.StaffGroup([p1, p2], symbol='brace')
                         score.insert(0.0, staffGroup)
-                        
+
                     if isinstance(structure, SystemicBarline):
                         SystBarline = score.getElementsByClass(layout.StaffGroup)[0] if len(score.getElementsByClass(layout.StaffGroup)) > 0 else None
                         if not SystBarline:
@@ -327,16 +327,16 @@ def translateToMusic21(tree, preserveStemDirection=False):
                             score.insert(0.0, staffGroup)
                         else:
                             SystBarline.addSpannedElements(part)
-                        
+
                 else: # next parts already exist
                     part = part.next()
                     measure['counter'] = part[-1].number
                     insertNewMeasure()
-                    
+
                 catchPartitioning = False
                 continue
         #(end grand staff)
-        
+
         # New line
         if isinstance(structure, Newline):
             # Clean-up
@@ -344,16 +344,16 @@ def translateToMusic21(tree, preserveStemDirection=False):
                 part.remove(part[-1]) # measure['current']
             catchPartitioning = True # trigger watch-state
             continue
-            
+
     # Clean-up
     if type(part[-1]) is stream.Measure and len(part[-1]) == 0:
         part.remove(part[-1]) # measure['current']
-    
+
     # Separate overlaps
     for partId, measureNo in makeVoices:
         p = score.getElementById(partId)
         p[measureNo].makeVoices() # ugly results when durations don't fit, automatically generated rests
-    
+
     # Automatic beams
     # if autoBeams:
     #     for p in score.parts: # echo TimeSignature declarations
@@ -363,10 +363,10 @@ def translateToMusic21(tree, preserveStemDirection=False):
     #                 ts = m.getElementsByClass(meter.TimeSignature)[0]
     #             else:
     #                 m.timeSignature = ts
-    #            
+    #
     #     for p in score.parts: # make auto-beams
     #         p.makeBeams(inPlace=True)
-    
+
     return score
 
 def setMetadata(m21stream, scoreTitle="Untitled", scoreComposer="Unknown Composer", scoreTempo=None, scoreInstruments=None, midiPrograms=False):
@@ -411,31 +411,31 @@ def writeStream(m21stream, format='midi', wrtpath=None, scoreTitle=None, scoreCo
     scoreComposer: str
     scoreTempo: int
     scoreInstruments: list[ str ]
-    
+
     Write out Music21 stream in a given format. If wrtpath is not specified
     the file is written in the current working directory (as 'untitled.ext'),
     or if wrtpath is a directory, 'untitled.ext' files are written there.
-    
+
     The possible output formats are
         musicxml lily(pond) midi
     """
     import os
     from music21.common import findFormat
-    
+
     fmt, ext = findFormat(format)
-    
+
     if not wrtpath:
         wrtpath = os.path.join(os.getcwd(), 'untitled' + ext)
     if os.path.isdir(wrtpath):
         wrtpath = os.path.join(wrtpath, 'untitled' + ext)
-    
+
     # Part writing (app.py: MyFrame.getOffsetScore)
     if isinstance(m21stream, stream.Part):
         import copy
         score = stream.Score()
         score.append(copy.deepcopy(m21stream))
         m21stream = score
-    
+
     # Metadata
     setMetadata(m21stream,
         scoreTitle=scoreTitle,
@@ -443,13 +443,13 @@ def writeStream(m21stream, format='midi', wrtpath=None, scoreTitle=None, scoreCo
         scoreTempo=scoreTempo,
         scoreInstruments=scoreInstruments,
         midiPrograms=False)
-                
+
     m21stream.write(format, wrtpath)
-    
+
 
 if __name__ == '__main__':
     import sys
-    
+
     if len(sys.argv) >= 2:
         t = parse(sys.argv[1])
         s = translateToMusic21(t)
@@ -458,4 +458,4 @@ if __name__ == '__main__':
         if len(sys.argv) == 3:
             writeStream(s, format=sys.argv[2])
     else:
-        print "Usage:\n\t$ python translateToMusic21.py [string] [format]\nOutput path is current working directory."
+        print "Usage:\n\t$ python translator.py [string] [format]\nOutput path is current working directory."
--- desktop/cavatina/write.py
+++ desktop/cavatina/write.py
@@ -11,24 +11,24 @@ def writeFiles(filename, outfilename, filedir=None):
     if filedir:
         filename = join(filedir, filename)
         outfilename = join(filedir, outfilename)
-    
+
     with open(outfilename, "wt") as fout:
         with open(filename, "rt") as fin:
             tree = parse(fin.read())
             fout.write( '\n'.join([str(n) for n in tree]) )
-            
+
     with open(filename, "rt") as fin:
         tree = parse(fin.read())
         score = translator.translateToMusic21(tree)
         translator.writeStream(score, format='text', wrtpath=filedir)
         translator.writeStream(score, format='midi', wrtpath=filedir)
         translator.writeStream(score, format='musicxml', wrtpath=filedir)
-            
+
 
 if __name__ == "__main__":
     import sys
     sys.path.append(dirname(abspath(__file__)))
-    
+
     if len(sys.argv) > 1:
         if len(sys.argv) == 2 and sys.argv[1] == '-h':
             print "Usage:\tpython write.py [input file path] [output directory]"
@@ -37,9 +37,8 @@ if __name__ == "__main__":
             if len(sys.argv) == 3:
                 filedir = sys.argv[2]
             else:
-                filedir = None
+                filedir = dirname(sys.argv[1]) # Output to same directory as input file
             outfilename = "out.txt"
             writeFiles(filename, outfilename, filedir)
     else:
         print "Usage:\tpython write.py [input file path] [output directory]"
-        
